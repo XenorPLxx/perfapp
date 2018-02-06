ae10c2e9b006afb671000cf5104be0cf8d19416b
Dawid Karczewski <d.karczewski@ideamotive.co>
2018-02-06 17:00:19 +0100

    Added recyclerlistview

--- src.js
+++ src.js
@@ -5,8 +5,14 @@ import {
   FlatList,
   Text,
   TouchableNativeFeedback,
-  TouchableOpacity
+  TouchableOpacity,
+  Dimensions
 } from "react-native";
+import {
+  RecyclerListView,
+  DataProvider,
+  LayoutProvider
+} from "recyclerlistview";
 
 const metrics = {
   // MENU
@@ -50,6 +56,24 @@ const metrics = {
     small: 10,
     large: 20,
     veryLarge: 25
+  },
+  container: {
+    justifyContent: "space-around",
+    alignItems: "center",
+    flex: 1,
+    backgroundColor: "#00a1f1"
+  },
+  containerGridLeft: {
+    justifyContent: "space-around",
+    alignItems: "center",
+    flex: 1,
+    backgroundColor: "#ffbb00"
+  },
+  containerGridRight: {
+    justifyContent: "space-around",
+    alignItems: "center",
+    flex: 1,
+    backgroundColor: "#7cbb00"
   }
 };
 
@@ -987,23 +1011,112 @@ class TransportPaymentsList extends Component<Props> {
   render() {
     const { currentData, queryNetworkStatus, queryRefetch } = this.props;
     console.log(currentData);
+    const { width } = Dimensions.get("window");
+    const { height } = Dimensions.get("window");
     return (
-      <FlatList
-        extraData={this.props.currentData}
-        data={currentData}
-        keyExtractor={this.keyExtractor}
-        ListHeaderComponent={this.renderFlatListHeader}
-        renderItem={this.renderTableRow}
-        style={styles.list}
-        contentContainerStyle={styles.listContent}
-        initialNumToRender={currentData.length}
-        refreshing={queryNetworkStatus === 4}
-        onRefresh={() => queryRefetch()}
-        getItemLayout={(data, index) => ({
-          length: 52,
-          offset: 52 * (index + 1),
-          index
-        })}
+      <View style={{ width: width - 2, height: height - 150 }}>
+        <RecycleTestComponent
+          currentData={currentData}
+          renderTableRow={this.renderTableRow}
+        />
+      </View>
+    );
+  }
+}
+
+const ViewTypes = {
+  FULL: 0,
+  HALF_LEFT: 1,
+  HALF_RIGHT: 2
+};
+
+let containerCount = 0;
+
+// eslint-disable-next-line
+class CellContainer extends React.Component {
+  constructor(args) {
+    super(args);
+    this._containerId = containerCount++;
+  }
+  render() {
+    return (
+      <View {...this.props}>
+        {this.props.children}
+        <Text>Cell Id: {this._containerId}</Text>
+      </View>
+    );
+  }
+}
+
+/** *
+ * To test out just copy this component and render in you root component
+ */
+// eslint-disable-next-line
+class RecycleTestComponent extends React.Component {
+  constructor(args) {
+    super(args);
+
+    const { width } = Dimensions.get("window");
+
+    // Create the data provider and provide method which takes in two rows of data and return if those two are different or not.
+    // THIS IS VERY IMPORTANT, FORGET PERFORMANCE IF THIS IS MESSED UP
+    const dataProvider = new DataProvider((r1, r2) => isEqual(r1, r2));
+
+    // Create the layout provider
+    // First method: Given an index return the type of item e.g ListItemType1, ListItemType2 in case you have variety of items in your list/grid
+    // Second: Given a type and object set the exact height and width for that type on given object, if you're using non deterministic rendering provide close estimates
+    // If you need data based check you can access your data provider here
+    // You'll need data in most cases, we don't provide it by default to enable things like data virtualization in the future
+    // NOTE: For complex lists LayoutProvider will also be complex it would then make sense to move it to a different file
+    this._layoutProvider = new LayoutProvider(
+      () => ViewTypes.FULL,
+      (type, dim) => {
+        switch (type) {
+          case ViewTypes.FULL:
+            dim.width = width;
+            dim.height = 52;
+            break;
+          default:
+            dim.width = 0;
+            dim.height = 0;
+        }
+      }
+    );
+
+    this._rowRenderer = this._rowRenderer.bind(this);
+
+    // Since component should always render once data has changed, make data provider part of the state
+    this.state = {
+      // dataProvider: dataProvider.cloneWithRows(this._generateArray(300)),
+      dataProvider: dataProvider.cloneWithRows(this.props.currentData)
+    };
+  }
+
+  _generateArray(n) {
+    const arr = new Array(n);
+    for (let i = 0; i < n; i++) {
+      arr[i] = i;
+    }
+    return arr;
+  }
+
+  // Given type and data return the view component
+  _rowRenderer(type, data) {
+    // You can return any view here, CellContainer has no special significance
+    switch (type) {
+      case ViewTypes.FULL:
+        return this.props.renderTableRow({ item: data });
+      default:
+        return null;
+    }
+  }
+
+  render() {
+    return (
+      <RecyclerListView
+        layoutProvider={this._layoutProvider}
+        dataProvider={this.state.dataProvider}
+        rowRenderer={this._rowRenderer}
       />
     );
   }
